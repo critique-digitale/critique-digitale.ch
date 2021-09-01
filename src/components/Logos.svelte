dba388f9f724f8fcc07f87155fd8365538faeeda
Moritz Mähr <moritz.maehr@gmail.com>
2021-09-01 15:40:50 +0200

    links added to logos

--- src/components/Logos.svelte
+++ src/components/Logos.svelte
@@ -1,32 +1,44 @@
 <section>
-	<aside>
-		<figure>
-			<img alt="Infoclio" src="images/logos/infoclio-ch_RGB.png" />
-		</figure>
-	</aside>
-	<aside>
-		<figure>
-			<img alt="Universität Bern" src="images/logos/Unibe_Logo_16pt_RGB_201807.png" />
-		</figure>
-	</aside>
-	<aside>
-		<figure>
-			<img alt="Université de Genève" src="images/logos/UNIGE70.gif" />
-		</figure>
-	</aside>
-	<aside>
-		<figure>
-			<img alt="DHLab" src="images/logos/dhlab-logo-amber.png" />
-		</figure>
-	</aside>
-	<aside>
-		<figure>
-			<img alt="University of Basel" src="images/logos/uni-basel-logo-ogtag.png" />
-		</figure>
-	</aside>
-	<aside>
-		<figure>
-			<img alt="dhcenter UNIL EPFL" src="images/logos/logo-dhcenter-unil-epfl.png" />
-		</figure>
-	</aside>
+	<a href="https://infoclio.ch/"
+		><aside>
+			<figure>
+				<img alt="Infoclio" src="images/logos/infoclio-ch_RGB.png" />
+			</figure>
+		</aside></a
+	>
+	<a href="https://www.unibe.ch/">
+		<aside>
+			<figure>
+				<img alt="Universität Bern" src="images/logos/Unibe_Logo_16pt_RGB_201807.png" />
+			</figure>
+		</aside>
+	</a>
+	<a href="https://www.unige.ch/">
+		<aside>
+			<figure>
+				<img alt="Université de Genève" src="images/logos/UNIGE70.gif" />
+			</figure>
+		</aside></a
+	>
+	<a href="https://dhlab.philhist.unibas.ch/">
+		<aside>
+			<figure>
+				<img alt="DHLab" src="images/logos/dhlab-logo-amber.png" />
+			</figure>
+		</aside></a
+	>
+	<a href="https://unibas.ch/">
+		<aside>
+			<figure>
+				<img alt="University of Basel" src="images/logos/uni-basel-logo-ogtag.png" />
+			</figure>
+		</aside></a
+	>
+	<a href="https://dhcenter-unil-epfl.com/">
+		<aside>
+			<figure>
+				<img alt="dhcenter UNIL EPFL" src="images/logos/logo-dhcenter-unil-epfl.png" />
+			</figure>
+		</aside></a
+	>
 </section>
