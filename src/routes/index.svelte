6b96c0ddb36781b167bf674c71d147cd63fdaf0e
Moritz Mähr <moritz.maehr@gmail.com>
2021-09-01 15:05:42 +0200

    new structure

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,10 +12,9 @@
 			/></a
 		>
 		<ul>
-			<li><a href="#program" class="secondary-font">Program</a></li>
-			<li><a href="#participate" class="secondary-font">Participate</a></li>
-			<li><a href="#faq" class="secondary-font">FAQ</a></li>
-			<li><a href="#about" class="secondary-font">About Us</a></li>
+			<li><a href="#concept" class="secondary-font">Concept</a></li>
+			<li><a href="#programme" class="secondary-font">Programme</a></li>
+			<li><a href="#organisation" class="secondary-font">Organisation</a></li>
 			<li>
 				🌍
 				<ul>
@@ -32,128 +31,120 @@
 	</p>
 </header>
 <main>
-	<section id="unconference">
-		<aside>
-			<h3>Subject</h3>
-			<p>
-				The humanities and social sciences - especially in the fields of Digital Studies and Digital
-				Humanities - have developed a methodological and critical reflection to analyze the "digital
-				turn" and its impact on both research and society at large. However, exchange on these
-				issues across disciplinary boundaries has remained limited.
-			</p>
-		</aside>
-		<aside>
-			<h3>Objective</h3>
-			<p>
-				The aim of the event, Unconference 'Digital Critique', is to invite academic as well as
-				professional experts from various disciplines in the humanities, social and data sciences to
-				spend two days discussing the challenges of digital criticism and new knowledge regimes of
-				the 21st century.
-			</p>
-		</aside>
-		<aside>
-			<h3>Format</h3>
-			<p>
-				The format chosen is an online "unconference": a format that encourages informal exchange,
-				participation and networking. The program of the day will be determined by the participants
-				in attendance (see for instance the concept of BarCamp). Discussions will take place in
-				parallel sessions and will be reported back in the form of collective reports. The
-				"unconference" will take place on October 21 and 22, 2021.
-			</p>
-		</aside>
+	<section id="concept">
+		<header>
+			<h2>Concept</h2>
+		</header>
+		<article>
+			<h3>Intro & Objectifs</h3>
+			<p>bla</p>
+		</article>
+		<article>
+			<h3>What is an unconference</h3>
+			<p>bla</p>
+		</article>
 	</section>
 	<hr />
 	<article id="program">
 		<header>
-			<h2>Program</h2>
+			<h2>Programme</h2>
 		</header>
-		<table>
-			<thead>
-				<tr>
-					<th />
-					<th>October 21</th>
-					<th />
-					<th>22 October</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>8:45</td>
-					<td>Point A</td>
-					<td />
-					<td />
-				</tr>
-				<tr>
-					<td>10:30</td>
-					<td>XYZ</td>
-					<td />
-					<td />
-				</tr>
-			</tbody>
-		</table>
+		<article>
+			<h3>Keynotes</h3>
+			<p>bla</p>
+		</article>
+		<article>
+			<h3>Programme</h3>
+			<table>
+				<thead>
+					<tr>
+						<th />
+						<th>October 21</th>
+						<th />
+						<th>22 October</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>8:45</td>
+						<td>Point A</td>
+						<td />
+						<td />
+					</tr>
+					<tr>
+						<td>10:30</td>
+						<td>XYZ</td>
+						<td />
+						<td />
+					</tr>
+				</tbody>
+			</table>
+		</article>
 	</article>
 	<hr />
-	<article id="participate">
+	<article id="organisation">
 		<header>
-			<h2>Participate</h2>
+			<h2>Organisation</h2>
 		</header>
-		<h3>Subscribe</h3>
-		<p>
-			Register for free at this <a
-				href="https://www.infoclio.ch/de/unconference-digital-critique-information-list-liste-dinformations-informationsliste"
-				>link</a
-			>.
-		</p>
-		<h3>Submit topic</h3>
-		<section>
-			<form action="https://formspree.io/f/mqkwlowj" method="POST">
-				<label>
-					Name
-					<input type="text" name="name" />
-				</label>
-				<label>
-					Email address
-					<input type="email" name="_replyto" />
-				</label>
-				<label>
-					Suggested topic
-					<textarea name="topic" />
-				</label>
-				<!-- your other form fields go here -->
-				<button type="submit">submit</button>
-			</form>
-		</section>
+		<article>
+			<h3>Registration</h3>
+			<p>
+				Register for free at this <a
+					href="https://www.infoclio.ch/de/unconference-digital-critique-information-list-liste-dinformations-informationsliste"
+					>link</a
+				>.
+			</p>
+		</article>
+		<article>
+			<h3>Pitching & Voting Procedures</h3>
+			<section>
+				<form action="https://formspree.io/f/mqkwlowj" method="POST">
+					<label>
+						Name
+						<input type="text" name="name" />
+					</label>
+					<label>
+						Email address
+						<input type="email" name="_replyto" />
+					</label>
+					<label>
+						Suggested topic
+						<textarea name="topic" />
+					</label>
+					<!-- your other form fields go here -->
+					<button type="submit">submit</button>
+				</form>
+			</section>
+		</article>
+		<article>
+			<h3>Languages</h3>
+			<p>bla</p>
+		</article>
+		<article>
+			<h3>FAQ</h3>
+			<details>
+				<summary>Is this event free of charge?</summary>
+				<p>Yes.</p>
+			</details>
+			<details>
+				<summary>How do I register in Big Blue Button?</summary>
+				<p>XYZ.</p>
+			</details>
+		</article>
 	</article>
 	<hr />
-	<article id="faq">
-		<header>
-			<h2>Frequently Asked Questions</h2>
-		</header>
-
-		<details>
-			<summary>Is this event free of charge?</summary>
-			<p>Yes.</p>
-		</details>
-		<details>
-			<summary>How do I register in Big Blue Button?</summary>
-			<p>XYZ.</p>
-		</details>
-	</article>
-
-	<hr />
-	<article id="about">
-		<header>
-			<h2>About us</h2>
-		</header>
-
-		<h3>Committee</h3>
-		<p>XYZ</p>
-		<h3>Committee</h3>
-		<p>
-			dhCenter UNIL-EPFL, Programme Doctoral en Études Numériques (PDEN), Digital Humanities at the.
-			Walter Benjamin Kolleg of the University of Bern, infoclio.ch, Digital Humanities Lab
-			University of Basel
-		</p>
-	</article>
-	<Logos />
 </main>
+
+<footer>
+	<header>
+		<h2>About us</h2>
+	</header>
+	<p>
+		dhCenter UNIL-EPFL, Programme Doctoral en Études Numériques (PDEN), Digital Humanities at the.
+		Walter Benjamin Kolleg of the University of Bern, infoclio.ch, Digital Humanities Lab University
+		of Basel
+	</p>
+	<Logos />
+	<h3>Contact</h3>
+	<p>info@critique-digitale.ch</p>
+</footer>
