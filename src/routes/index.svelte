016c513614e82eaea28c387758b2500c3f145a95
Moritz Mähr <moritz.maehr@gmail.com>
2021-09-01 15:41:16 +0200

    form removed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -92,24 +92,7 @@
 		</p>
 
 		<h3>Pitching & Voting Procedures</h3>
-		<section>
-			<form action="https://formspree.io/f/mqkwlowj" method="POST">
-				<label>
-					Name
-					<input type="text" name="name" />
-				</label>
-				<label>
-					Email address
-					<input type="email" name="_replyto" />
-				</label>
-				<label>
-					Suggested topic
-					<textarea name="topic" />
-				</label>
-				<!-- your other form fields go here -->
-				<button type="submit">submit</button>
-			</form>
-		</section>
+		<p>bla</p>
 
 		<h3>Languages</h3>
 		<p>bla</p>
