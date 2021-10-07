14262e89d59396fbf6b90b583f46d19e43a81a0e
Moritz Mähr <moritz.maehr@gmail.com>
2021-10-07 10:36:10 +0200

    don't show panels with no title

--- src/lib/components/Panelslot.svelte
+++ src/lib/components/Panelslot.svelte
@@ -6,13 +6,19 @@
 			title: 'This could be your topic!',
 			room: 'https://bbb.ch-open.ch/b/mor-h4k-hun',
 			pad: 'https://annuel2.framapad.org/p/61kp8ko7ia-9q4d?lang=en'
+		},
+		{
+			slot: '1',
+			title: '',
+			room: 'https://bbb.ch-open.ch/b/mor-h4k-hun',
+			pad: 'https://annuel2.framapad.org/p/61kp8ko7ia-9q4d?lang=en'
 		}
 	];
 </script>
 
 {#if panels.length > 0}
 	{#each panels as { slot, title, room, pad }}
-		{#if slot == panelslot}
+		{#if slot == panelslot && title != ''}
 			<br /><a href={room}>{title}</a> <a href={pad}>📝 Etherpad</a>
 		{/if}
 	{/each}
