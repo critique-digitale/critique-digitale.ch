38ed0f5fe64575eb0accad0e9c1ca3dbc9e8d08f
Moritz Mähr <moritz.maehr@gmail.com>
2021-10-08 15:34:28 +0200

    subsession added

--- src/lib/components/Panelslot.svelte
+++ src/lib/components/Panelslot.svelte
@@ -2,151 +2,176 @@
 	export let panelslot = '1';
 	let panels = [
 		{
-			slot: '1',
+			panel: '1',
+			subsession: '1',
 			title: 'This could be your topic!',
 			room: 'https://bbb.ch-open.ch/b/mor-h4k-hun',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_1_subsession_1-9q5l?lang=en'
 		},
 		{
-			slot: '1',
+			panel: '1',
+			subsession: '2',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_1_subsession_2-9q5l?lang=en'
 		},
 		{
-			slot: '1',
+			panel: '1',
+			subsession: '3',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_1_subsession_3-9q5l?lang=en'
 		},
 		{
-			slot: '1',
+			panel: '1',
+			subsession: '4',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_1_subsession_4-9q5o?lang=en'
 		},
 		{
-			slot: '1',
+			panel: '1',
+			subsession: '5',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_1_subsession_5-9q5o?lang=en'
 		},
 		{
-			slot: '2',
+			panel: '2',
+			subsession: '1',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_2_subsession_1-9q5o?lang=en'
 		},
 		{
-			slot: '2',
+			panel: '2',
+			subsession: '2',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_2_subsession_2-9q5o?lang=en'
 		},
 		{
-			slot: '2',
+			panel: '2',
+			subsession: '3',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_2_subsession_3-9q5o?lang=en'
 		},
 		{
-			slot: '2',
+			panel: '2',
+			subssession: '4',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_2_subsession_4-9q5o?lang=en'
 		},
 		{
-			slot: '2',
+			panel: '2',
+			subsession: '5',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_2_subsession_5-9q5o?lang=en'
 		},
 		{
-			slot: '3',
+			panel: '3',
+			subsession: '1',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_3_subsession_1-9q5o?lang=en'
 		},
 		{
-			slot: '3',
+			panel: '3',
+			subsession: '2',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_3_subsession_2-9q5o?lang=en'
 		},
 		{
-			slot: '3',
+			panel: '3',
+			subsession: '3',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_3_subsession_3-9q5o?lang=en'
 		},
 		{
-			slot: '3',
+			panel: '3',
+			subsession: '4',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_3_subsession_4-9q5o?lang=en'
 		},
 		{
-			slot: '3',
+			panel: '3',
+			subsession: '5',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_3_subsession_5-9q5o?lang=en'
 		},
 		{
-			slot: '4',
+			panel: '4',
+			subsession: '1',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_4_subsession_1-9q5o?lang=en'
 		},
 		{
-			slot: '4',
+			panel: '4',
+			subession: '2',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_4_subsession_2-9q5o?lang=en'
 		},
 		{
-			slot: '4',
+			panel: '4',
+			subession: '3',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_4_subsession_3-9q5o?lang=en'
 		},
 		{
-			slot: '4',
+			panel: '4',
+			subession: '4',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_4_subsession_4-9q5o?lang=en'
 		},
 		{
-			slot: '4',
+			panel: '4',
+			subession: '5',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_4_subsession_5-9q5o?lang=en'
 		},
 		{
-			slot: '5',
+			panel: '5',
+			subsession: '1',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_5_subsession_1-9q5o?lang=en'
 		},
 		{
-			slot: '5',
+			panel: '5',
+			subsession: '2',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_5_subsession_2-9q5o?lang=en'
 		},
 		{
-			slot: '5',
+			panel: '5',
+			subsession: '3',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_5_subsession_3-9q5o?lang=en'
 		},
 		{
-			slot: '5',
+			panel: '5',
+			subsession: '4',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_5_subsession_4-9q5o?lang=en'
 		},
 		{
-			slot: '5',
+			panel: '5',
+			subsession: '5',
 			title: '',
 			room: '',
 			pad: 'https://semestriel.framapad.org/p/cd_panel_5_subsession_5-9q5o?lang=en'
@@ -155,8 +180,8 @@
 </script>
 
 {#if panels.length > 0}
-	{#each panels as { slot, title, room, pad }}
-		{#if slot == panelslot && title != ''}
+	{#each panels as { panel, title, room, pad }}
+		{#if panel == panelslot && title != ''}
 			<br /><a href={room}>{title}</a> <a href={pad}>📝 Etherpad</a>
 		{/if}
 	{/each}
