369fa94b0e8ed75aebe4fb256f759236de65c271
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2022-02-17 17:43:27 +0100

    style tables and images of keynote speakers

--- src/lib/components/MdLayout.svelte
+++ src/lib/components/MdLayout.svelte
@@ -14,4 +14,23 @@
     height: auto;
     margin-left: calc((100vw - var(--width-content) - 1rem)/2 * -1);
 }
+:global(.float) {
+    margin: auto;
+    display: block;
+}
+@media (min-width: 500px) {
+    :global(.float) {
+        float: left;
+        margin: 0 1em 1em 0;
+    }
+}
+:global(table) {
+    white-space: break-spaces;
+}
+:global(.first-center table thead th:first-child, table tr td:first-child) {
+    text-align: center;
+}
+:global(.first-center table td, table th, table tr) {
+    text-align: left;
+}
 </style>
