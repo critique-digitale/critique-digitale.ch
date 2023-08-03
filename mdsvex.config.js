5f5e7a648b9097524324a133805fa0ab43a97df0
Moritz Mähr <moritz.maehr@gmail.com>
2023-08-03 23:07:15 +0200

    upgrade to sveltekit 1.0

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,12 +1,22 @@
-const config = {
+import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
 
 	smartypants: {
 		dashes: 'oldschool'
 	},
+
 	layout: '/src/lib/components/MdLayout.svelte',
 	remarkPlugins: [],
+	rehypePlugins: [],
+	extensions: ['.svelte.md', '.md', '.svx'],
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [],
 	rehypePlugins: []
-};
+});
 
 export default config;
